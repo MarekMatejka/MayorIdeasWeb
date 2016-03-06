6df60e11df433b0bf3fc62fc88385e494fb63863
MarekMatejkaKCL <marek.matejka@kcl.ac.uk>
2016-03-06 17:49:54 +0000

    Implementing comment and map to IdeaDetail page

--- dist/js/idea_details.js
+++ dist/js/idea_details.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $('#dataTable_wrapper').hide();
 
 	//var id = sessionStorage.getItem("id");
-    var id = 4;
+    var id = 10;
 
     $.get(url+"/image/get/idea/"+id, function(data) {
         var ideaImageIds = JSON.parse(data);
@@ -18,6 +18,13 @@ $(document).ready(function() {
         }
     }, "text");
 
+    var commentCounter = 0;
+    $.get(url+"/comment/idea/"+id, function(data) {
+        var comments = JSON.parse(data);
+        console.log(comments);
+        addNComments(comments, 5);
+    }, "text");
+
     $.get(url+"/idea/"+id+"?user_id="+getCurrentID(), function(data) {
         var idea = JSON.parse(data);
         console.log(idea);
@@ -27,6 +34,7 @@ $(document).ready(function() {
         setupComments(idea.numOfComments);
         setupState(idea.ideaState);
         setupIdeaDetails(idea);
+        setupMap(idea);
     }, "text");
 
     function setupCategory(categoryID, categoryName) {
@@ -60,7 +68,7 @@ $(document).ready(function() {
     }
 
     function setupIdeaDetails(idea) {
-        $("#ideaDetails").append("<p>by "+idea.authorName+", "+idea.dateCreated+"</p>");
+        $("#ideaDetails").append("<p style='color: #757575'>by "+idea.authorName+", "+idea.dateCreated+"</p>");
         $("#ideaDetails").append("<p><b>Description:</b> "+idea.description+"</p>");
     }
 
@@ -72,5 +80,76 @@ $(document).ready(function() {
             case 4: return '<i class="fa fa-road fa-lg" style="align: center;color:#757575"></i>';
             default: return '<i class="fa fa-leaf fa-lg" style="align: center;"></i>';
         } 
+    }
+
+    function addNComments(comments, showCommentsCount) {
+        var showAddMore = false;
+        var end = 0;
+        if ((comments.length-commentCounter) > showCommentsCount) {
+            end = commentCounter+showCommentsCount;
+            showAddMore = true;
+        } else {
+            end = comments.length;
+            showAddMore = false;
+        }
+
+
+        for (var i = commentCounter; i < end; i++) {
+            $('#comments').append(createComment(comments[i]));
+            commentCounter++;
+        }
+
+        if (showAddMore) {
+            var commentsLeft = comments.length-commentCounter;
+            var showCount = commentsLeft >= showCommentsCount ? showCommentsCount : commentsLeft;
+            $('#comments').append(createShowMoreCommentsButton(showCount));
+            setupShowMoreCommentsButtonOnClick(comments, showCount);
+        }
+
+        if (comments.length == 0) {
+            $('#comments').append('<p style="text-align: center">No comments added for this idea</p>');   
+        }
+    }
+
+    function createShowMoreCommentsButton(showCount) {
+        return '<button id="showMoreComments" type="button" class="btn btn-link" style="width: 100%">'+
+                    'Show '+showCount+' more comments'+
+                '</button>';
+    }
+
+    function setupShowMoreCommentsButtonOnClick (comments, showCount) {
+        $("#showMoreComments").off("click").on("click", function(event) {
+            $('#showMoreComments').remove();
+            addNComments(comments, showCount);
+        });
+    }
+
+    function createComment(comment) {
+        var footerColor = comment.isByCitizen ? "#5f8dd3" : "#FFD54F";
+        var footerTextColor = comment.isByCitizen ? "white" : "black";
+        return '<div class="panel panel-default">'+
+                    '<div class="panel-body">'+
+                        '<p>"'+comment.text+'"</p>'+
+                    '</div>'+
+                    '<div class="panel-footer" style="background-color: '+footerColor+'; color: '+footerTextColor+'">'+
+                        comment.userName+', '+comment.dateCreated+
+                    '</div>'+
+                '</div>'
+    }
+
+    function setupMap(idea) {
+        var map = new GMaps({
+          div: '#map',
+          lat: idea.latitude,
+          lng: idea.longitude,
+          zoom: 16  
+        });
+        map.addMarker({
+          lat: idea.latitude,
+          lng: idea.longitude,
+          infoWindow: {
+            content: '<p>'+idea.title+'</p>'
+          }
+        });
     }
 });	
